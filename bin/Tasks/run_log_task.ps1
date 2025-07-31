8a430747da9a9cb228bd2d3ef8305aab4195e733
paneves1 <148705439+paneves1@users.noreply.github.com>
2025-07-31 12:36:13 +0100

    run log collector silent
    Donwloads from git and executes log collector silent

---
+++ bin/Tasks/run_log_task.ps1
@@ -0,0 +1,14 @@
+﻿# Silent Collector Launcher
+
+$exeName = "Silent_evo_Collector.exe"
+$downloadUrl = "https://github.com/paneves1/log_collector/raw/refs/heads/main/bin/$exeName"
+$destinationPath = "C:\Windows\Temp\$exeName"
+
+# Kill if already running
+Get-Process -Name "Silent_evo_Collector" -ErrorAction SilentlyContinue | Stop-Process -Force
+
+# Download the executable
+Invoke-WebRequest -Uri $downloadUrl -OutFile $destinationPath -UseBasicParsing
+
+# Run the executable (non-blocking, no window)
+Start-Process -FilePath $destinationPath -WindowStyle Hidden
